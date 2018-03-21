ee584d0ac6dc2e8ed539155dd17eb9d21cd216a4
vigneshnomercy <37579861+vigneshnomercy@users.noreply.github.com>
2018-03-21 16:34:23 +0530

    Create file1.cpp

---
+++ file1.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main(){
+int a;
+cout<<"enter your number";
+cin>>a;
+if(a==0)
+{
+cout<<"value is zero";
+}else if(a>=1){
+cout<<"value is possitive";
+}else{
+cout<<"value is negative";}
+getch();
+}
